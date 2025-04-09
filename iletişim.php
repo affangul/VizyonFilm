4fea43e4d538e846705181833141a0384dd9cc2f
affan <affan.gul33@gmail.com>
2025-04-09 19:41:46 +0300

    evet

---
+++ iletişim.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>İletişim</title>
+  <link rel="stylesheet" href="css/iletişimstyle.css">
+  <link rel="stylesheet" href="css/genelstyle.css">
+</head>
+<body>
+  <header>
+    <h1>Vizyon Film</h1>
+    <nav>
+      <a href="index.php">Ana Sayfa</a>
+      <a href="filmler.php">Filmler</a>
+      <a href="hakkımızda.php">Hakkımızda</a>
+      <a href="iletişim.php">İletişim</a>
+    </nav>
+  </header>
+
+
+
+  <footer>
+  <nav>
+      <a href="index.php">Ana Sayfa</a>
+      <a href="filmler.php">Filmler</a>
+      <a href="hakkımızda.php">Hakkımızda</a>
+      <a href="iletişim.php">İletişim</a>
+    </nav>
+    <p>&copy; 2025 Vizyon Film. Tüm hakları saklıdır.</p>
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ana Sayfa</title>
+  <link rel="stylesheet" href="css/indexstyle.css">
+  <link rel="stylesheet" href="css/genelstyle.css">
+</head>
+<body>
+  <header>
+    <h1>Vizyon Film</h1>
+    <nav>
+      <a href="index.php">Ana Sayfa</a>
+      <a href="filmler.php">Filmler</a>
+      <a href="hakkımızda.php">Hakkımızda</a>
+      <a href="iletişim.php">İletişim</a>
+    </nav>
+  </header>
+
+  <main>
+  <article class="film">
+  <div class="film-icerik">
+    <div class="film-sol">
+      <h2>Daredevil: Born Again</h2>
+      <img src="img/daredevil.jpg">
+      
+    </div>
+    <div class="film-sag">
+      <div class="puan"><strong>Vizyon Film:</strong> 8.5 / 10</div>
+      <div class="puan"><strong>IMDb Puanı:</strong> 7.9 / 10</div>
+    </div>
+    
+  </div>
+  <p class="film-aciklama">Matt Murdock, çocukken radyoaktif bir maddeyle kaza geçirerek kör olmuş bir avukattır. Görme yetisini kaybetmiş olmasına rağmen, diğer duyuları diğer insanların hayal bile edemeyeceği seviyelere ulaşmıştır. Matt, gündüzleri Nelson & Murdock ofisinde bir avukatken, geceleri New York'taki Hell's Kitchen'da kanunsuz Daredevil olarak faaliyet gösterir.</p>
+  <a href="#" class="incele-btn">Daha fazlası...</a>  
+</article>
+
+<article class="film">
+  <div class="film-icerik">
+    <div class="film-sol">
+      <h2>The Pitt</h2>
+      <img src="img/thePitt.jpg">
+      
+    </div>
+    <div class="film-sag">
+      <div class="puan"><strong>Vizyon Film:</strong> 8.5 / 10</div>
+      <div class="puan"><strong>IMDb Puanı:</strong> 7.9 / 10</div>
+    </div>
+    
+  </div>
+  <p class="film-aciklama">The Pitt, günümüz Amerika'sındaki sağlık çalışanlarının karşılaştığı zorlukların, Pensilvanya, Pittsburgh'daki modern bir hastanede çalışan ön cephe kahramanlarının merceğinden görüldüğü bir incelemesidir.</p>
+  <a href="#" class="incele-btn">Daha fazlası...</a>  
+</article>
+
+<article class="film">
+  <div class="film-icerik">
+    <div class="film-sol">
+      <h2>Minecraft</h2>
+      <img src="img/minecraft.jpg">
+      
+    </div>
+    <div class="film-sag">
+      <div class="puan"><strong>Vizyon Film:</strong> 8.5 / 10</div>
+      <div class="puan"><strong>IMDb Puanı:</strong> 7.9 / 10</div>
+    </div>
+    
+  </div>
+  <p class="film-aciklama">Bir Minecraft Filmi, gizemli bir geçitten geçerek bilmedikleri bir dünyaya adım atan bir grup uyumsuzun hikayesini konu ediyor. Bambaşka karaktere sahip olan Garrett Garrison, Henry, Natalie ve Dawn, gizemli bir kapıdan tuhaf bir dünyaya çekildiklerinde günlük hayatlarından koparlar. Onlar artık neredeyse sınırsız hayal gücünün hakim olduğu garip, kübik bir ülke olan Overworld’dedir. Eve geri dönüş yolunu bulmak için bu alışılmadık dünyaya hakim olmaları ve kendilerini zombiler ve yaban domuzları gibi tehditlere karşı savunmaları gerekir. Bu süreçte dört maceracıya deneyimli bir zanaatkar olan Steve eşlik eder. Bu yolculuk, onlara cesaret ve yaratıcılıklarını keşfetme şansı verecektir.</p>
+  <a href="#" class="incele-btn">Daha fazlası...</a>  
+</article>
+
+</main>
+  <footer>
+  <nav>
+      <a href="index.php">Ana Sayfa</a>
+      <a href="filmler.php">Filmler</a>
+      <a href="hakkımızda.php">Hakkımızda</a>
+      <a href="iletişim.php">İletişim</a>
+    </nav>
+    <p>&copy; 2025 Vizyon Film. Tüm hakları saklıdır.</p>
+  </footer>
+</body>
+</html>
